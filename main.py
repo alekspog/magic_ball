38f8e71e9a64535724a172043d179fe9f66f00a3
alekspog <aleksey.pogibelev@gmail.com>
2016-07-09 00:11:32 +0300

    user can insert first page in main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ driver.implicitly_wait(10)
 
 page = Page(driver)
 first_page_link = "https://market.yandex.ru/catalog/54933/list?hid=90580&how=dpop&in-stock=1"
-
-items_id = page.get_items_id(first_page_link, page_numbers=2)
+pages = 2
+items_id = page.get_items_id(first_page_link, page_numbers=pages)
 page.get_items_cards(items_id)
 
 driver.close()
